cd437f85adedc0aab632ae1b0217120e80391e69
Gabriel Ferreras Garrucho <gabriel@MacBook-de-Gabriel.local>
2020-11-10 16:32:25 +0100

    Starting reading a parameter file

--- RNASeqPipeline.sh
+++ RNASeqPipeline.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-if [ $# -eq 0 ]
+if [ $# -ne 1 ]
 then
     echo ""
-    echo "    Usage: bash RNASeqPipeline.sh <params_file> <annotation> <sample>"
+    echo "    Usage: bash RNASeqPipeline.sh <params_file>"
     echo ""
     echo "    params_file: imput file with the parameters."
     echo ""
     exit
 fi
 
+PARAMS=$1
 
+EXP= $(grep experiment_name: $PARAMS | awk '{ print $2 }')
+echo "experiment name = "$EXP
