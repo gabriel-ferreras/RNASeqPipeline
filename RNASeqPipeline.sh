c2fb2ed63559df90115062a94bd58bf2ac7051b1
Gabriel Ferreras Garrucho <gabriel@MacBook-de-Gabriel.local>
2020-11-10 16:13:51 +0100

    Adicion de usage a RNASeqPipeline.sh

--- RNASeqPipeline.sh
+++ RNASeqPipeline.sh
@@ -1,1 +1,13 @@
 #! /bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo ""
+    echo "    Usage: bash RNASeqPipeline.sh <params_file> <annotation> <sample>"
+    echo ""
+    echo "    params_file: imput file with the parameters."
+    echo ""
+    exit
+fi
+
+
