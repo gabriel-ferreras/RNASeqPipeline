d4c26b89693e78d1e4f2e1aea0537e1b088cb6f8
gabriel-ferreras <gabfergar@molsysbio.us.es>
2020-11-17 16:30:55 +0100

    Creating Workspace and Index of Reference Genome

--- RNASeqPipeline.sh
+++ RNASeqPipeline.sh
@@ -13,7 +13,11 @@ fi
 
 #Reading parameters file.
 PARAMS=$1
-
+echo ""
+echo "======================"
+echo "| LOADING PARAMETERS |"
+echo "======================"
+echo ""
 WORK_DIR=$(grep working_directory: $PARAMS | awk '{ print $2 }')
 echo "      Working directory = "$WORK_DIR
 
@@ -23,22 +27,27 @@ echo "      Experiment name = "$EXP
 NUM_SAMPLES=$(grep number_samples: $PARAMS | awk '{ print $2 }')
 echo "      Number samples = "$NUM_SAMPLES
 
-ANNOTATION=$(grep annotation: $PARAMS | awk '{ print $2 }')
+ANNOTATION=$(grep path_annotation: $PARAMS | awk '{ print $2 }')
 echo "      Annotation in "$ANNOTATION
 
-GENOME=$(grep genome: $PARAMS | awk '{ print $2 }')
+GENOME=$(grep path_genome: $PARAMS | awk '{ print $2 }')
 echo "      Genome in "$GENOME
 
-i=1
-while [ $i -le $NUM_SAMPLES ]
+SAMPLES=()
+i=0
+while [ $i -lt $NUM_SAMPLES ]
 do
-       	SAMPLE_$i=$(grep sample_$i: $PARAMS | awk '{ print $2 }')
-	echo "      Sample $i in = "${SAMPLE_$i}
+	j=$(( $i + 1 ))
+       	SAMPLES[$i]=$(grep path_sample_$j: $PARAMS | awk '{ print $2 }')
+	echo "      Sample $j in ${SAMPLES[$i]}"
         ((i++))
 done
-
-
 #Preparing working workspace.
+echo ""
+echo "======================"
+echo "| CREATING WORKSPACE |"
+echo "======================"
+echo ""
 cd $WORK_DIR
 mkdir $EXP
 cd $EXP
@@ -53,23 +62,73 @@ done
 cd ..
 
 #Copying the data.
-mv $ANNOTATION $WORK_DIR/$EXP/annotation/annotation.gtf
-mv $GENOME $WORK_DIR/$EXP/genome/genome.fa
+cp $ANNOTATION $WORK_DIR/$EXP/annotation/annotation.gtf
+cp $GENOME $WORK_DIR/$EXP/genome/genome.fa
 i=1
 while [ $i -le $NUM_SAMPLES ]
 do
-        ${SAMPLE_$i} $WORK_DIR/samples/sample_$i/sample_$i.fastqc
+	j=$((i - 1))
+        cp ${SAMPLES[j]} $WORK_DIR/$EXP/samples/sample_$i/sample_$i.fastqc.gz
         ((i++))
 done
 
 
-#Creating genome index
-#cd $WORK_DIR/$EXP/annotation
-#extract_splice_sites.py annotation.gtf>splice_sites.ss
-#extract_exons.py annotation.gtf>exons.exon
-#cd ../genome
-#hisat2-build --ss ../annotation/splice_sites.ss --exon ../annotation/exons.exon genoma.fa index
+#Creating genome index.
+echo ""
+echo "========================="
+echo "| CREATING GENOME INDEX |"
+echo "========================="
+echo ""
+cd $WORK_DIR/$EXP/annotation
+extract_splice_sites.py annotation.gtf > splice_sites.ss
+extract_exons.py annotation.gtf > exons.exon
+cd ../genome
+hisat2-build --ss ../annotation/splice_sites.ss --exon ../annotation/exons.exon genome.fa index
 
+#Quality analysis of samples fastq files by FASTQC.
+echo ""
+echo "=============================="
+echo "| QUALITY CONTROL OF SAMPLES |"
+echo "=============================="
+echo ""
+
+cd ../samples
+while [ $i -le $NUM_SAMPLES ]
+do
+        cd sample_$i
+	gzip -d sample_$i.fastqc.gz
+	fastqc sample_$i.fastqc
+	cd ..
+	((i++))
+done
+
+#Mapping of lectures to reference genome by HISAT2.
+echo ""
+echo "========================="
+echo "|   MAPPING BY HISAT2   |"
+echo "========================="
+echo ""
+
+
+
+while [ $i -le $NUM_SAMPLES ]
+do
+	cd sample_$i
+	if [ -f SRR5602506_2.fastq ]
+then
+   fastqc SRR5602506_1.fastq
+   fastqc SRR5602506_2.fastq
+
+   hisat2 --dta -x ../../genome/index -1 SRR5602506_1.fastq -2 SRR5602506_2.fastq -S lux6_2.sam
+else
+   fastqc SRR5602506_1.fastq
+
+   hisat2 --dta -x ../../genome/index -U SRR5602506_1.fastq -S lux6_2.sam
+fi
+	hisat2 --dta -x ../../genome/index -U sample_$i.fastqc -S sample_$i.sam
+	cd ..
+ 	((i++))
+done
 
 
 echo "Analysis complete :)"
